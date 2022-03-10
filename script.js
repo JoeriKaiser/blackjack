8aa8a88458588b5dc01f803d1f47e0276c2c983b
JoeriKaiser <joeri.kaiser@hotmail.com>
2022-03-10 15:38:47 +0100

    Fonctionnel sans bouton

--- script.js
+++ script.js
@@ -1,73 +1,73 @@
-const confirmation = confirm("Voulez-vous commencer à jouer?");
-let cartes1 = [getRandom(), getRandom()];
-let cartes2 = [getRandom(), getRandom()];
-if (confirmation) {
-  console.log("Cartes du joueur numéro 1 : ", cartes1[0], "et", cartes1[1]);
-  console.log("Cartes du joueur numéro 2 : ", cartes2[0], "et une carte cachée");
-} else {
-  alert("Dommage !");
-}
+let cartes1 = 0;
+let cartes2 = 0;
+
 const totalCartes1 = document.querySelector("#cartes-1");
 const totalCartes2 = document.querySelector("#cartes-2");
-totalCartes1.innerHTML = `${getValue(cartes1)}`;
-totalCartes2.innerHTML = `${cartes2[0]}, et une carte cachée`;
+const winnerH1 = document.querySelector("#winner");
+
+function compareScores(score1, score2) {
+  if (score1 > score2) {
+    winnerH1.innerHTML = "Joueur 1 a gagner";
+  } else if (score1 === score2) {
+    winnerH1.innerHTML = "Egalite";
+  } else if (score1 < score2 ) {
+    winnerH1.innerHTML = "Joueur 2 a gagner";
+  }
+}
+console.log(compareScores())
+
 function getRandom() {
   return Math.floor(Math.random(1, 10) *10 + 1);
 }
+
 function getValue(hand) {
   let sum = 0;
   for (let i = 0; i < hand.length; i++) {
     sum = sum + hand[i];
   }
-  return sum
+  return sum;
+};
+
+function startGame() {
+  cartes1 = [getRandom(), getRandom()];
+  cartes2 = [getRandom(), getRandom()];
 }
-function hit() {
-  if (getValue(cartes1) < 21) {
+if (window.confirm("Voulez-vous commencer et piocher deux cartes?")){
+  startGame();
+  totalCartes1.innerHTML = `${cartes1}, Pour un total de : ${getValue(cartes1)}`
+  totalCartes2.innerHTML = `${cartes2}, Pour un total de : ${getValue(cartes2)}`
+} else {
+  alert("Dommage!");
+  location.reload();
+}
+console.log("Player hand 1 :" + cartes1);
+console.log("Player hand 1 value : " + getValue(cartes1));
+console.log("Player hand 2 :" + cartes2);
+console.log("Player hand 2 value : " + getValue(cartes2));
+
+while (getValue(cartes1) < 21) {
+  if (window.confirm(`Votre total est de ${getValue(cartes1)} vous souhaiter tirer une carte supplémentaire?`)) {
     cartes1.push(getRandom());
-    console.log(getValue(cartes1));
-    totalCartes1.innerHTML = `${getValue(cartes1)}`;
+    totalCartes1.innerHTML = `${cartes1}, Pour un total de : ${getValue(cartes1)}`
   } else {
-    console.log("test")
-  }
-  while (getValue(cartes1) > 21) {
-    const response = confirm("Le joueur 1 à perdu voulez-vous rejouer?");
-    if (response === true) {
-      window.location.reload();
+    break;
     }
-    totalCartes1.innerHTML = `${getValue(cartes1)}`;
-  }
-  while (getValue(cartes1) === 21) {
-    const response = confirm("Le joueur 1 à fait un Blackjack, voulez-vous rejouer?");
-    if (response === true) {
-      window.location.reload();
-    }
-    totalCartes1.innerHTML = `${getValue(cartes1)}`;
-  }
+}
 
-  while (getValue(cartes2) <= 16) {
-    cartes2.push(getRandom());
-    totalCartes2.innerHTML = `${getValue(cartes2)}`;
-  }
-  while (getValue(cartes2) > 17 && getValue(cartes2) < 21) {
-    console.log(`Le joueur 2 s'arrete à ${getValue(cartes2)}`);
-    totalCartes2.innerHTML = `${getValue(cartes2)}`;
-    break;
-  }
+if (getValue(cartes1) > 21) {
+  alert("Game Over!")
+  window.location.reload()
+} else if (getValue(cartes1) === 21) {
+  alert("BLACKJACK !!");
+}
 
-  while (getValue(cartes2) > 21 ) {
-    const response = confirm("Le joueur 2 a perdu, voulez-vous rejouer?");
-    if (response === true) {
-      window.location.reload();
-    }
-    totalCartes2.innerHTML = `${getValue(cartes2)}`;
-    break;
-  }
-  while (getValue(cartes2) === 21 ) {
-    const response = confirm("Le joueur 2 à fait un Blackjack, voulez-vous rejouer?");
-    if (response === true) {
-      window.location.reload();
-    }
-    totalCartes2.innerHTML = `${getValue(cartes2)}`;
-    break;
-  }
+if (getValue(cartes2) <= 16) {
+  cartes2.push(getRandom());
+  totalCartes2.innerHTML = `${cartes2}, Pour un total de : ${getValue(cartes2)}`
+  compareScores(getValue(cartes1), getValue(cartes2));
+} else if (getValue(cartes2) > 21) {
+  alert("Le Joueur 2 a eu plus que 21, Donc le joueur 1 remporte la partie");
+  winnerH1.innerHTML = "Joueur 1 a gagner car le Joueur 2 à d2";
+} else {
+  compareScores(getValue(cartes1), getValue(cartes2));
 }
